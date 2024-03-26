ce57a684ab8e24a857c79c9d5ba7548632e4e093
Hariprasath <prasathmhari565@gmail.com>
2024-03-26 09:57:29 +0530

    versioning

---
+++ springboot-microservices/1-restful-web-service/src/main/java/com/myproject/restfulwebservice/versioning/Name.java
@@ -0,0 +1,30 @@
+package com.myproject.restfulwebservice.versioning;
+
+public class Name {
+    
+    private String firstName;
+    private String secondName;
+
+    public Name(String firstName, String secondName) {
+        super();
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    @Override
+    public String toString() {
+        return "Name [firstName=" + firstName + ", secondName=" + secondName + "]";
+    }
+
+    
+
+
+}
---
+++ springboot-microservices/1-restful-web-service/src/main/java/com/myproject/restfulwebservice/versioning/VersioningPersonController.java
@@ -0,0 +1,57 @@
+package com.myproject.restfulwebservice.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+
+    // URI VERSIONING
+
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondtVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // REQUEST PARAMETER VERSIONING
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondtVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // CUSTOM HEADERS VERSIONING (REQUEST HEADER)
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondtVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // MEDIA TYPE VERSIONING (ACCEPT HEADER)
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondtVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+}
